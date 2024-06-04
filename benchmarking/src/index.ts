67032caa08c7f605f535f49e4ca7d1aa10df6a92
Neel Sudhakaran <nsudhakaran679@student.fuhsd.org>
2024-06-03 21:54:37 -0700

    feat(benchmarking): Completed Sizing & Export Logic

--- benchmarking/src/index.ts
+++ benchmarking/src/index.ts
@@ -1,21 +1,50 @@
 import { hrtime } from 'node:process'
-import { readdir as readDir } from 'node:fs/promises';
+import { readdir as readDir, stat, writeFile } from 'node:fs/promises';
 import { execute } from './utils/index.js';
 
-const IMAGE_DIR = '../images';
+const IMAGE_DIR = './images';
 const COMPRESSION_QUALITY = 80;
 
+type CompressionStats = [number, number, number, number, number];
 
-for (const file of await readDir(IMAGE_DIR)) {
-    if (!file.endsWith('.png')) continue;
+let files = await readDir(IMAGE_DIR);
+
+files = files.filter(file => file.endsWith('.png'));
+
+const data = Array.from({ length: files.length }, () => [0, 0, 0, 0, 0] as CompressionStats);
+
+for (const file of files) {
+    const index = parseInt(file.split('.')[0]!) - 1;
+    const filePng = `${IMAGE_DIR}/${file}`;
+    console.log(index);
+
+    const { size: sizePng } = await stat(filePng);
+    data[index]![0] = sizePng;
 
     const startWebp = hrtime();
-    await execute(`cwebp -q ${COMPRESSION_QUALITY} ${IMAGE_DIR}/${file} -o ${IMAGE_DIR}/${file.replace('.png', '.webp')}`)
+    const fileWebp = `${IMAGE_DIR}/${file.replace('.png', '.webp')}`
+    await execute(`cwebp -q ${COMPRESSION_QUALITY} ${filePng} -o ${fileWebp}`)
     const endWebp = hrtime(startWebp);
-    console.log(file, "to WebP finished in", endWebp[0] + (endWebp[1] / 1e6), "seconds.")
-    
+    const millisecondsWebp = endWebp[0] + (endWebp[1] / 1e6);
+    console.log(file, "to WebP finished in", millisecondsWebp, "milliseconds.")
+    data[index]![3] = millisecondsWebp;
+
+    const { size: sizeWebp } = await stat(fileWebp);
+    data[index]![1] = sizeWebp; 
+
     const startAvif = hrtime();
-    await execute(`avifenc -q ${COMPRESSION_QUALITY} ${IMAGE_DIR}/${file} ${IMAGE_DIR}/${file.replace('.png', '.avif')}`)
+    const fileAvif = `${IMAGE_DIR}/${file.replace('.png', '.avif')}`
+    await execute(`avifenc -q ${COMPRESSION_QUALITY} ${filePng} ${fileAvif}`)
     const endAvif = hrtime(startAvif);
-    console.log(file, "to AVIF finished in", endAvif[0] + (endAvif[1] / 1e6), "milliseconds.")
+    const millisecondsAvif = endAvif[0] + (endAvif[1] / 1e6);
+    console.log(file, "to AVIF finished in", millisecondsAvif, "milliseconds.")
+    data[index]![4] = millisecondsAvif;
+
+    const { size: sizeAvif } = await stat(fileAvif);
+    data[index]![2] = sizeAvif;;
+
 }
+
+console.log(data)
+
+writeFile('data.csv', data.map(row => row.join(',')).join('\n'));
